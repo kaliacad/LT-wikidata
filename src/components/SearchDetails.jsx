ea75a7db8c72d7703c6ccbfeb993e3dc7309b010
Landry Bitege <lan-bit@debian.landry>
2024-04-30 18:48:17 +0200

    Add Tailwind in project

--- src/components/SearchDetails.jsx
+++ src/components/SearchDetails.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function SearchDetails() {
-  return <div>seach</div>;
+export default function SearchDetails({ value }) {
+  return (
+    <div>
+      <div>
+        <h3>{value.label}</h3>
+        <h3>
+          <a href={value.concepturi}>{value.id}</a>
+        </h3>
+      </div>
+    </div>
+  );
 }
