b4b925165256a3e18fd5db015f4e8ee5ba3cdc28
Landry Bitege <lan-bit@debian.landry>
2024-04-30 18:32:19 +0200

    create SearchDetail

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import SearchDetails from "./SearchDetails";
 
 export default function Body({ data }) {
   console.log(data);
   return (
     <div>
       <h3>{`${data["search-continue"]} résultats pour la recherche de "${data.searchinfo.search}"`}</h3>
+      {data.search.map((search, i) => (
+        <SearchDetails key={i} value={search} />
+      ))}
     </div>
   );
 }
---
+++ src/components/SearchDetails.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchDetails() {
+  return <div>seach</div>;
+}
