14c03f1f85d79880343da2ab8bafed497d7ebeaf
Landry Bitege <lan-bit@debian.landry>
2024-04-30 13:45:54 +0200

    feat Heder && install hook-form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Header() {
-  return <div>Header</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          name="Seach"
+          id="seach"
+          {...register("text", {
+            required: true,
+            maxLength: 20,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+        />
+        <button type="submit">Seach</button>
+      </form>
+    </div>
+  );
 }
