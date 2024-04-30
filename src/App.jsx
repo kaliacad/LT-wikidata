cede934d8d388d307725404ce7726cc41ad20127
Landry Bitege <lan-bit@debian.landry>
2024-04-30 17:16:08 +0200

    feat API

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("null");
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const url = "https://www.wikidata.org/w/api.php";
+    const params = {
+      action: "wbsearchentities",
+      format: "json",
+      search: input,
+      language: "fr",
+    };
+
+    axios
+      .get(url, { params })
+      .then((response) => {
+        const responseData = response.data;
+        const finalData = responseData.search.map((entity) => ({
+          label: entity.label,
+          description: entity.description,
+          url: entity.url,
+          wd_id: entity.title,
+        }));
+        setData(finalData);
+        console.log(finalData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error);
+      });
+  }, []);
 
   return (
-    <>
-      <Header />
-      <Body />
-    </>
+    <div>
+      <div>
+        <Header setInput={setInput} />
+      </div>
+      {/* <Body /> */}
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-export default function Header() {
+export default function Header({ setInput }) {
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = (search) => {
+    setInput(search.text);
   };
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          name="Seach"
-          id="seach"
+          name="Search"
+          id="search"
           {...register("text", {
             required: true,
             maxLength: 20,
             pattern: /^[A-Za-z]+$/i,
           })}
         />
-        <button type="submit">Seach</button>
+        <button type="submit">Search</button>
       </form>
     </div>
   );
